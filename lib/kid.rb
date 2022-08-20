c5b2ec534dd908ab0bb6e4d649887c7b43167f47
Jobsidney <jobsidney67@gmail.com>
2022-08-20 20:22:50 +0300

    DOne

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,18 @@
 require_relative './dance_module'
 require_relative './meta_dancing_module'
+
+class Kid
+    include FancyDance::InstanceMethods
+    extend FancyDance::ClassMethods
+
+    attr_accessor :name
+    def initialize(name)
+        @name = name
+    end
+end
+
+
+angelina=Kid.new("Angelina")
+
+p "#{angelina.name} says: #{angelina.twirl}"
+p Kid.metadata
